bba5ddb7c7d37d6012079d61df4b3f0966c2ac90
Marco <marcochavezf@gmail.com>
2021-04-30 17:10:56 -0400

    move app bar as custom app bar

--- frontend/components/layout/CustomAppBar.tsx
+++ frontend/components/layout/CustomAppBar.tsx
@@ -1,29 +1,25 @@
-import { Badge, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
-import Container from '@material-ui/core/Container';
-import Divider from '@material-ui/core/Divider';
-import Drawer from '@material-ui/core/Drawer';
+import { Badge, InputBase } from '@material-ui/core';
+import AppBar from '@material-ui/core/AppBar';
 import IconButton from '@material-ui/core/IconButton';
-import List from '@material-ui/core/List';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
 import { createStyles, fade, makeStyles, Theme, useTheme } from '@material-ui/core/styles';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 import AccountCircle from '@material-ui/icons/AccountCircle';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import ListAltIcon from '@material-ui/icons/ListAlt';
-import PostAddIcon from '@material-ui/icons/PostAdd';
+import MenuIcon from '@material-ui/icons/Menu';
+import MoreIcon from '@material-ui/icons/MoreVert';
+import SearchIcon from '@material-ui/icons/Search';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import clsx from 'clsx';
 import { useRouter } from 'next/router';
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { useCart } from '../../lib/cartState';
 import { useDrawer } from '../../lib/drawerState';
-import SignOut from '../SignOut';
+import Search from '../Search';
 import { useUser } from '../User';
-import CustomAppBar from './CustomAppBar';
 
 const drawerWidth = 240;
-
+const menuId = 'primary-search-account-menu';
+const mobileMenuId = 'primary-search-account-menu-mobile';
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
@@ -141,9 +137,7 @@ const useStyles = makeStyles((theme: Theme) =>
   }),
 );
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-export default function PersistentDrawerLeft({ children }) {
+export default function CustomAppBar() {
   const classes = useStyles();
   const theme = useTheme();
   const { drawerOpen, openDrawer, closeDrawer } = useDrawer();
@@ -178,116 +172,81 @@ export default function PersistentDrawerLeft({ children }) {
     handleMenuClose();
   }
 
-  const menuId = 'primary-search-account-menu';
-  const renderMenu = (
-    <Menu
-      anchorEl={anchorEl}
-      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-      id={menuId}
-      keepMounted
-      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-      open={isMenuOpen}
-      onClose={handleMenuClose}
-    >
-      {user ? [
-        <MenuItem key="1" onClick={navigateTo('/orders')}>My Orders</MenuItem>,
-        <SignOut key="2" postSignOut={handleMenuClose} />
-      ] : [
-        <MenuItem key="1" onClick={navigateTo('/signin')}>Sign In</MenuItem>,
-        <MenuItem key="2" onClick={navigateTo('/signup')}>Sign Up</MenuItem>,
-        <MenuItem key="3" onClick={navigateTo('/reset_password')}>Reset Password</MenuItem>,
-      ]}
-    </Menu>
-  );
-
-  const mobileMenuId = 'primary-search-account-menu-mobile';
-  const cartMenuItem = (
-    <MenuItem key="cart" onClick={toggleCart}>
-      <IconButton aria-label={`show ${getTotalCartItems(user)} cart items`} color="inherit">
-        <Badge badgeContent={getTotalCartItems(user)} color="secondary">
-          <ShoppingCartIcon />
-        </Badge>
-      </IconButton>
-      <p>Cart</p>
-    </MenuItem>
-  );
-  const profileMenuItem = (
-    <MenuItem key="profile" onClick={handleProfileMenuOpen}>
+  return (<AppBar
+    position="fixed"
+    className={clsx(classes.appBar, {
+      [classes.appBarShift]: drawerOpen,
+    })}
+  >
+    <Toolbar>
       <IconButton
-        aria-label="account of current user"
-        aria-controls="primary-search-account-menu"
-        aria-haspopup="true"
         color="inherit"
+        aria-label="open drawer"
+        onClick={openDrawer}
+        edge="start"
+        className={clsx(classes.menuButton, drawerOpen && classes.hide)}
       >
-        <AccountCircle />
+        <MenuIcon />
       </IconButton>
-      <p>Profile</p>
-    </MenuItem>
-  );
-  const menuItems = user ? [cartMenuItem, profileMenuItem] : [profileMenuItem];
-  const renderMobileMenu = (
-    <Menu
-      anchorEl={mobileMoreAnchorEl}
-      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-      id={mobileMenuId}
-      keepMounted
-      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-      open={isMobileMenuOpen}
-      onClose={handleMobileMenuClose}
-    >
-      {menuItems}
-    </Menu>
-  );
 
-  return (
-    <div className={classes.root}>
-      
-      <CustomAppBar />
+      <Typography variant="h6" noWrap>
+        Online Store
+      </Typography>
 
-      <Drawer
-        className={classes.drawer}
-        variant="persistent"
-        anchor="left"
-        open={drawerOpen}
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-      >
-        <div className={classes.drawerHeader}>
-          <IconButton onClick={closeDrawer}>
-            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
-          </IconButton>
+      <Search />
+
+      <div className={classes.search}>
+        <div className={classes.searchIcon}>
+          <SearchIcon />
         </div>
-        <Divider />
-        <List>
-          <ListItem button onClick={navigateTo('/products')}>
-            <ListItemIcon><ListAltIcon /></ListItemIcon>
-            <ListItemText primary='Products' />
-          </ListItem>
-          {user && <ListItem button onClick={navigateTo('/sell')}>
-            <ListItemIcon><PostAddIcon /></ListItemIcon>
-            <ListItemText primary='Sell' />
-          </ListItem>}
-        </List>
-        <Divider />
-      </Drawer>
+        <InputBase
+          placeholder="Search…"
+          classes={{
+            root: classes.inputRoot,
+            input: classes.inputInput,
+          }}
+          inputProps={{ 'aria-label': 'search' }}
+        />
+      </div>
 
-      <main
-        className={clsx(classes.content, {
-          [classes.contentShift]: drawerOpen,
-        })}
-      >
-        <div className={classes.drawerHeader} />
+      <div className={classes.grow} />
+      <div className={classes.sectionDesktop}>
 
-        <Container maxWidth="md">
-          {children}
-        </Container>
+        {user && (
+          <IconButton
+            aria-label={`show ${getTotalCartItems(user)} cart items`}
+            onClick={toggleCart}
+            color="inherit">
+            <Badge badgeContent={getTotalCartItems(user)} color="secondary">
+              <ShoppingCartIcon />
+            </Badge>
+          </IconButton>
+        )}
 
-      </main>
+        <IconButton
+          edge="end"
+          aria-label="account of current user"
+          aria-controls={menuId}
+          aria-haspopup="true"
+          onClick={handleProfileMenuOpen}
+          color="inherit"
+        >
+          <AccountCircle />
+        </IconButton>
+      </div>
 
-      {renderMobileMenu}
-      {renderMenu}
+      <div className={classes.sectionMobile}>
+        <IconButton
+          aria-label="show more"
+          aria-controls={mobileMenuId}
+          aria-haspopup="true"
+          onClick={handleMobileMenuOpen}
+          color="inherit"
+        >
+          <MoreIcon />
+        </IconButton>
+      </div>
 
-    </div>
-  );
+    </Toolbar>
+  </AppBar>)
 }
