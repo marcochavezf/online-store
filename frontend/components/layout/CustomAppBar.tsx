273373d23f18a2d56bcce4a6fe4a49b67e540a4d
Marco <marcochavezf@gmail.com>
2021-05-01 13:44:17 -0500

    organize CustomAppBaer and PersistenDrawer

--- frontend/components/layout/CustomAppBar.tsx
+++ frontend/components/layout/CustomAppBar.tsx
@@ -1,7 +1,8 @@
-import { Badge, InputBase } from '@material-ui/core';
+import { Badge, InputBase, Menu } from '@material-ui/core';
 import AppBar from '@material-ui/core/AppBar';
 import IconButton from '@material-ui/core/IconButton';
-import { createStyles, fade, makeStyles, Theme, useTheme } from '@material-ui/core/styles';
+import MenuItem from '@material-ui/core/MenuItem';
+import { createStyles, fade, makeStyles, Theme } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import AccountCircle from '@material-ui/icons/AccountCircle';
@@ -15,6 +16,7 @@ import { useState } from 'react';
 import { useCart } from '../../lib/cartState';
 import { useDrawer } from '../../lib/drawerState';
 import Search from '../Search';
+import SignOut from '../SignOut';
 import { useUser } from '../User';
 
 const drawerWidth = 240;
@@ -45,45 +47,8 @@ const useStyles = makeStyles((theme: Theme) =>
     hide: {
       display: 'none',
     },
-    drawer: {
-      width: drawerWidth,
-      flexShrink: 0,
-    },
-    drawerPaper: {
-      width: drawerWidth,
-    },
-    drawerHeader: {
-      display: 'flex',
-      alignItems: 'center',
-      padding: theme.spacing(0, 1),
-      // necessary for content to be below app bar
-      ...theme.mixins.toolbar,
-      justifyContent: 'flex-end',
-    },
-    content: {
-      flexGrow: 1,
-      padding: theme.spacing(3),
-      transition: theme.transitions.create('margin', {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-      }),
-      marginLeft: -drawerWidth,
-    },
-    contentShift: {
-      transition: theme.transitions.create('margin', {
-        easing: theme.transitions.easing.easeOut,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
-      marginLeft: 0,
-    },
     grow: {
       flexGrow: 1,
-    },
-    title: {
-      display: 'none',
-      [theme.breakpoints.up('sm')]: {
-        display: 'block',
-      },
     },
     search: {
       position: 'relative',
@@ -139,7 +104,6 @@ const useStyles = makeStyles((theme: Theme) =>
 
 export default function CustomAppBar() {
   const classes = useStyles();
-  const theme = useTheme();
   const { drawerOpen, openDrawer, closeDrawer } = useDrawer();
   const user = useUser();
   const router = useRouter();
@@ -148,13 +112,17 @@ export default function CustomAppBar() {
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = useState<null | HTMLElement>(null);
 
-  const isMenuOpen = Boolean(anchorEl);
-  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
-
   const getTotalCartItems = (user) => user && user.cart.reduce((tally, cartItem) => tally + (cartItem.product ? cartItem.quantity : 0), 0);
   const handleProfileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
   };
+  const handleMobileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
   const handleMobileMenuClose = () => {
     setMobileMoreAnchorEl(null);
   };
@@ -162,15 +130,72 @@ export default function CustomAppBar() {
     setAnchorEl(null);
     handleMobileMenuClose();
   };
-  const handleMobileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
-    setMobileMoreAnchorEl(event.currentTarget);
-  };
   const navigateTo = href => e => {
     e.preventDefault()
     router.push(href);
     closeDrawer();
     handleMenuClose();
   }
+
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      {user ? [
+        <MenuItem key="1" onClick={navigateTo('/orders')}>My Orders</MenuItem>,
+        <SignOut key="2" postSignOut={handleMenuClose} />
+      ] : [
+        <MenuItem key="1" onClick={navigateTo('/signin')}>Sign In</MenuItem>,
+        <MenuItem key="2" onClick={navigateTo('/signup')}>Sign Up</MenuItem>,
+        <MenuItem key="3" onClick={navigateTo('/reset_password')}>Reset Password</MenuItem>,
+      ]}
+    </Menu>
+  );
+
+  const cartMenuItem = (
+    <MenuItem key="cart" onClick={toggleCart}>
+      <IconButton aria-label={`show ${getTotalCartItems(user)} cart items`} color="inherit">
+        <Badge badgeContent={getTotalCartItems(user)} color="secondary">
+          <ShoppingCartIcon />
+        </Badge>
+      </IconButton>
+      <p>Cart</p>
+    </MenuItem>
+  );
+  const profileMenuItem = (
+    <MenuItem key="profile" onClick={handleProfileMenuOpen}>
+      <IconButton
+        aria-label="account of current user"
+        aria-controls="primary-search-account-menu"
+        aria-haspopup="true"
+        color="inherit"
+      >
+        <AccountCircle />
+      </IconButton>
+      <p>Profile</p>
+    </MenuItem>
+  );
+
+  const menuItems = user ? [cartMenuItem, profileMenuItem] : [profileMenuItem];
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      {menuItems}
+    </Menu>
+  );
 
   return (<AppBar
     position="fixed"
@@ -248,5 +273,9 @@ export default function CustomAppBar() {
       </div>
 
     </Toolbar>
+
+    {renderMobileMenu}
+    {renderMenu}
+
   </AppBar>)
 }
--- frontend/components/layout/PersistentDrawerLeft.tsx
+++ frontend/components/layout/PersistentDrawerLeft.tsx
@@ -1,24 +1,18 @@
-import { Badge, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
+import { ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
 import Container from '@material-ui/core/Container';
 import Divider from '@material-ui/core/Divider';
 import Drawer from '@material-ui/core/Drawer';
 import IconButton from '@material-ui/core/IconButton';
 import List from '@material-ui/core/List';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
-import { createStyles, fade, makeStyles, Theme, useTheme } from '@material-ui/core/styles';
-import AccountCircle from '@material-ui/icons/AccountCircle';
+import { createStyles, makeStyles, Theme, useTheme } from '@material-ui/core/styles';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import ListAltIcon from '@material-ui/icons/ListAlt';
 import PostAddIcon from '@material-ui/icons/PostAdd';
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import clsx from 'clsx';
 import { useRouter } from 'next/router';
-import React, { useState } from 'react';
-import { useCart } from '../../lib/cartState';
+import React from 'react';
 import { useDrawer } from '../../lib/drawerState';
-import SignOut from '../SignOut';
 import { useUser } from '../User';
 import CustomAppBar from './CustomAppBar';
 
@@ -28,26 +22,6 @@ const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       display: 'flex',
-    },
-    appBar: {
-      transition: theme.transitions.create(['margin', 'width'], {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-      }),
-    },
-    appBarShift: {
-      width: `calc(100% - ${drawerWidth}px)`,
-      marginLeft: drawerWidth,
-      transition: theme.transitions.create(['margin', 'width'], {
-        easing: theme.transitions.easing.easeOut,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    hide: {
-      display: 'none',
     },
     drawer: {
       width: drawerWidth,
@@ -80,164 +54,21 @@ const useStyles = makeStyles((theme: Theme) =>
       }),
       marginLeft: 0,
     },
-    grow: {
-      flexGrow: 1,
-    },
-    title: {
-      display: 'none',
-      [theme.breakpoints.up('sm')]: {
-        display: 'block',
-      },
-    },
-    search: {
-      position: 'relative',
-      borderRadius: theme.shape.borderRadius,
-      backgroundColor: fade(theme.palette.common.white, 0.15),
-      '&:hover': {
-        backgroundColor: fade(theme.palette.common.white, 0.25),
-      },
-      marginRight: theme.spacing(2),
-      marginLeft: 0,
-      width: '100%',
-      [theme.breakpoints.up('sm')]: {
-        marginLeft: theme.spacing(3),
-        width: 'auto',
-      },
-    },
-    searchIcon: {
-      padding: theme.spacing(0, 2),
-      height: '100%',
-      position: 'absolute',
-      pointerEvents: 'none',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    inputRoot: {
-      color: 'inherit',
-    },
-    inputInput: {
-      padding: theme.spacing(1, 1, 1, 0),
-      // vertical padding + font size from searchIcon
-      paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-      transition: theme.transitions.create('width'),
-      width: '100%',
-      [theme.breakpoints.up('md')]: {
-        width: '20ch',
-      },
-    },
-    sectionDesktop: {
-      display: 'none',
-      [theme.breakpoints.up('md')]: {
-        display: 'flex',
-      },
-    },
-    sectionMobile: {
-      display: 'flex',
-      [theme.breakpoints.up('md')]: {
-        display: 'none',
-      },
-    },
   }),
 );
-
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 export default function PersistentDrawerLeft({ children }) {
   const classes = useStyles();
   const theme = useTheme();
-  const { drawerOpen, openDrawer, closeDrawer } = useDrawer();
+  const { drawerOpen, closeDrawer } = useDrawer();
   const user = useUser();
   const router = useRouter();
-  const { toggleCart } = useCart();
 
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = useState<null | HTMLElement>(null);
-
-  const isMenuOpen = Boolean(anchorEl);
-  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
-
-  const getTotalCartItems = (user) => user && user.cart.reduce((tally, cartItem) => tally + (cartItem.product ? cartItem.quantity : 0), 0);
-  const handleProfileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleMobileMenuClose = () => {
-    setMobileMoreAnchorEl(null);
-  };
-  const handleMenuClose = () => {
-    setAnchorEl(null);
-    handleMobileMenuClose();
-  };
-  const handleMobileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
-    setMobileMoreAnchorEl(event.currentTarget);
-  };
   const navigateTo = href => e => {
     e.preventDefault()
     router.push(href);
     closeDrawer();
-    handleMenuClose();
   }
-
-  const menuId = 'primary-search-account-menu';
-  const renderMenu = (
-    <Menu
-      anchorEl={anchorEl}
-      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-      id={menuId}
-      keepMounted
-      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-      open={isMenuOpen}
-      onClose={handleMenuClose}
-    >
-      {user ? [
-        <MenuItem key="1" onClick={navigateTo('/orders')}>My Orders</MenuItem>,
-        <SignOut key="2" postSignOut={handleMenuClose} />
-      ] : [
-        <MenuItem key="1" onClick={navigateTo('/signin')}>Sign In</MenuItem>,
-        <MenuItem key="2" onClick={navigateTo('/signup')}>Sign Up</MenuItem>,
-        <MenuItem key="3" onClick={navigateTo('/reset_password')}>Reset Password</MenuItem>,
-      ]}
-    </Menu>
-  );
-
-  const mobileMenuId = 'primary-search-account-menu-mobile';
-  const cartMenuItem = (
-    <MenuItem key="cart" onClick={toggleCart}>
-      <IconButton aria-label={`show ${getTotalCartItems(user)} cart items`} color="inherit">
-        <Badge badgeContent={getTotalCartItems(user)} color="secondary">
-          <ShoppingCartIcon />
-        </Badge>
-      </IconButton>
-      <p>Cart</p>
-    </MenuItem>
-  );
-  const profileMenuItem = (
-    <MenuItem key="profile" onClick={handleProfileMenuOpen}>
-      <IconButton
-        aria-label="account of current user"
-        aria-controls="primary-search-account-menu"
-        aria-haspopup="true"
-        color="inherit"
-      >
-        <AccountCircle />
-      </IconButton>
-      <p>Profile</p>
-    </MenuItem>
-  );
-  const menuItems = user ? [cartMenuItem, profileMenuItem] : [profileMenuItem];
-  const renderMobileMenu = (
-    <Menu
-      anchorEl={mobileMoreAnchorEl}
-      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-      id={mobileMenuId}
-      keepMounted
-      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-      open={isMobileMenuOpen}
-      onClose={handleMobileMenuClose}
-    >
-      {menuItems}
-    </Menu>
-  );
 
   return (
     <div className={classes.root}>
@@ -284,10 +115,6 @@ export default function PersistentDrawerLeft({ children }) {
         </Container>
 
       </main>
-
-      {renderMobileMenu}
-      {renderMenu}
-
     </div>
   );
 }
